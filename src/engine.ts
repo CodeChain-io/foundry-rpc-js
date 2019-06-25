951f4410c47fde5840624b0d3b775188a4f5db93
Seulgi Kim <seulgi.kim@codechain.io>
2019-06-25 10:41:50 +0900

    Implement engine.getCustomActionData()

--- src/engine.ts
+++ src/engine.ts
@@ -24,4 +24,15 @@ export default class Engine extends RpcBase {
         const response = await this.call({ method, id });
         return response.result;
     }
+
+    async getCustomActionData(
+        params: { handlerId: number; bytes: string; blockNumber?: number | null },
+        id?: string | number | null
+    ): Promise<number> {
+        const method = "engine_getCustomActionData";
+        const { handlerId, bytes } = params;
+        const blockNumber = params.blockNumber == null ? null : params.blockNumber;
+        const response = await this.call({ method, id }, handlerId, bytes, blockNumber);
+        return response.result;
+    }
 }
